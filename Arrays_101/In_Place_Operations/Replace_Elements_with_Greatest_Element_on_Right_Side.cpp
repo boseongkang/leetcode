95a863780359d9b2f69eacb7be1e07efc9233b2f
bkang9801 <okboseong@gmail.com>
2022-01-03 23:20:31 -0800

    01/03/22

---
+++ Arrays_101/In_Place_Operations/Replace_Elements_with_Greatest_Element_on_Right_Side.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+
+        int end = -1;
+        int n = arr.size();
+        vector<int> new_arr(n);
+        new_arr[n - 1] = -1;
+        int cnt = n - 2;
+        for(int i = n - 1; i >= 0; i--)
+        {
+            if(arr[i] > end)
+                end = arr[i];
+            if(cnt >= 0)
+                new_arr[cnt] = end;
+            cnt--;
+        }
+        return new_arr;
+        
+    }
+};
