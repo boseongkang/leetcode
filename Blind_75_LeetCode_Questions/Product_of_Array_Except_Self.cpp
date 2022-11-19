54b0f06003185181f2ed5a6d999c4fd8f412fbd2
boseongkang <okboseong@gmail.com>
2022-11-18 23:37:36 -0800

    success

--- Blind_75_LeetCode_Questions/Product_of_Array_Except_Self.cpp
+++ Blind_75_LeetCode_Questions/Product_of_Array_Except_Self.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums)
-    {
+    vector<int> productExceptSelf(vector<int>& nums) {
+        
         int n = nums.size();
-        vector<int> result(n);
-        vector<int> temp_arr(n);
-        vector<int> temp(n);
-        for(int i = 0; i < nums.size(); i++)
+        int temp = 1;
+        vector<int> arr(n);
+        
+        for (int i = 0; i < n; i++)
         {
-            temp_arr[i] = 1;
-            if(temp_arr[i] == nums[i])
-            {
-               temp[i] = nums[i+1] * nums[i+2]; 
-            }
-            else 
-            {
-                temp[i] = nums[i] * nums[i+1];
-                result[i] = temp_arr[i+1] * nums[i];
-    
-            }
+            arr[i] = temp;
+            temp = temp * nums[i];
         }
-        return result;
+        
+        temp = 1;
+        for(int i = n - 1; i >= 0; i--)
+        {
+            arr[i] = arr[i] * temp;
+            temp = temp * nums[i];
+        }
+        return arr;
     }
 };
-// ERROR
-// Tried to make switch case with if statement, but getting worse.
-// Fix tomorrow.
