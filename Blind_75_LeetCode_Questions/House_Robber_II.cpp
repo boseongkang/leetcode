3df3c6143c73aab1bfa9d6753a6173131356d8d8
boseongkang <okboseong@gmail.com>
2024-06-26 15:52:03 -0700

    q213

--- Blind_75_LeetCode_Questions/House_Robber_II.cpp
+++ Blind_75_LeetCode_Questions/House_Robber_II.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-    int rob(const vector<int>& nums) 
+    int rob_func(vector<int>& nums, int start, int last) 
     {
         int first = 0;
         int second = 0;
         int temp = 0;
 
-        for(int num: nums)
+        for(int i =  start; i <= last; ++i)
         {
-            temp = max(first, second);
-            first = second + num;
+            temp = max(first + nums[i], second);
+            first = second;
             second = temp;
         }
-        return max(first, second);
+        return temp;
     }
 
-    int result(vector<int>& nums)
+    int rob(vector<int>& nums)
     {
         int cnt = nums.size();
-        if(cnt == 1)
-            return nums[0];
-        if(cnt == 2)
-            return max(nums[0], nums[1]);
-        
-        vector<int>adjacent_first(nums.begin() + 1, nums.end());
-        vector<int>adjacent_last(nums.begin(), nums.end() - 1);
+        if (cnt == 1) return nums[0];
+        if (cnt == 2) return max(nums[0], nums[1]);
 
-        
+        return max(rob_func(nums, 1, cnt - 1), rob_func(nums, 0, cnt - 2));
     }
-
 };
-
-
