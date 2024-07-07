c86dd4b8e0e26bc19b5626f08d815c3857db009a
boseongkang <okboseong@gmail.com>
2024-07-07 15:30:44 -0700

    finally.. understand the question

--- Graph_Problems_for_practice/Q684_Redundant_Connection/Redundant_Connection.cpp
+++ Graph_Problems_for_practice/Q684_Redundant_Connection/Redundant_Connection.cpp
@@ -1,61 +1,54 @@
-#include <vector>
-
-using namespace std;
-
 class Solution {
 private:
-    vector<int> parent; // Parent array to keep track of the roots
-    vector<int> rank;   // Rank array to keep track of tree heights
+    vector<int> parent;
+    vector<int> rank;
 
-    // Find function with path compression
-    int find(int node) {
-        if (parent[node] != node) {
-            parent[node] = find(parent[node]); // Path compression
-        }
-        return parent[node];
+    int find(int node)
+    {
+        if(parent[node] != node) // if node is not parent node, make that node as a parent; 
+            parent[node] = find(parent[node]);
+        return parent[node]; // if node is parent node, just return it. 
     }
-
-    // Union function with union by rank
-    bool unionNodes(int node1, int node2) {
-        int root1 = find(node1);
+    
+    bool unionFind(int node1, int node2)
+    {
+        int root1 = find(node1); // make variable root1 & root2 parent node to compare 
         int root2 = find(node2);
 
-        if (root1 != root2) { // Only union if they are in different sets
-            if (rank[root1] > rank[root2]) {
-                parent[root2] = root1; // Attach shorter tree under the root of the taller tree
-            } else if (rank[root1] < rank[root2]) {
-                parent[root1] = root2; // Attach shorter tree under the root of the taller tree
-            } else {
-                parent[root2] = root1; // If equal, attach and increase the rank
+        if(root1 != root2) // if root1 and root2 is same, it means parent node is same, also means this is not a tree; cycle
+        {
+            if(rank[root1] == rank[root2]) // if the rank(depth) is same, put the node under the parent node and increase rank(depth) 1
+            {
+                parent[root1] = root2;
                 rank[root1] += 1;
             }
+            else if(rank[root1] > rank[root2]) // if the root[rank1] > rank[root2] the depth of rank1 is big, so make node under the parent
+                parent[root2] = root1; 
             return true;
         }
         return false;
     }
 
-public:
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n = edges.size();
-        parent.resize(n + 1);  // Resize parent array to n + 1
-        rank.resize(n + 1, 1); // Resize rank array to n + 1 and initialize to 1
 
-        // Initialize the parent array
-        for (int i = 1; i <= n; ++i) {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) 
+    {
+        int n = edges.size();
+        parent.resize(n+1);
+        rank.resize(n+1, 1);
+
+        for(int i = 0; i <= n; ++i)
+        {
             parent[i] = i;
         }
 
-        // Iterate through each edge to find the redundant connection
-        for (const auto& edge : edges) {
+        for(auto& edge:edges)
+        {
             int u = edge[0];
             int v = edge[1];
-
-            if (!unionNodes(u, v)) { // If unionNodes returns false, there's a cycle
-                return {u, v}; // Return the edge causing the cycle
-            }
+            if(!unionFind(u,v))
+                return {u,v};
         }
-
-        return {}; // In case no redundant edge is found (though the problem guarantees there is one)
+    return {}; 
     }
 };
-
